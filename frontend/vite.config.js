7d1114025d2924348819ddf9a64406c02db05161
TULASI <tulasiindkmr@gmail.com>
2025-11-13 17:42:11 +0530

    Delete frontend directory

--- frontend/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// base path MUST match your GitHub repo name: 'dsr'
-export default defineConfig({
-  plugins: [react()],
-  base: "/dsr/"
-});
